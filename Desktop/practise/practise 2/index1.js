2db7bb0cf5d4b6cc43db1052f6d6d6fbc9b99f87
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2026-04-23 09:03:07 +0530

    pollyfill for flat

--- Desktop/practise/practise 2/index1.js
+++ Desktop/practise/practise 2/index1.js
@@ -776,3 +776,26 @@ let object2 ={
 
 // let printObject = printfullObject.myBind(object,"karnataka")
 // printObject("gurodi")
+
+const arr=[1,2,[[3,4]]];
+// const output=arr.flat(2);//depth  of the array by default depth will be 1
+// console.log(output)//returns new copy of the array
+
+Array.prototype.myFlat =function (depth){
+let res =[];
+if(!Array.isArray(this)){
+  throw new Error("flat is not a function")
+}
+this.forEach((el)=>{
+  if(Array.isArray(el) && depth > 0){
+    res.push(...el.myFlat(depth-1))
+  }
+  else{
+    res.push(el)
+  }
+})
+return res;
+
+}
+const output=arr.myFlat(2)
+console.log(output)
